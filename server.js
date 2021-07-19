aebae7fbeae72ede577035d1f4745db9e4c92f67
Aaron Kurowski <aaron12342@att.net>
2021-07-19 15:00:11 -0500

    Some preliminary crud requests added with html form for post request. Body-parser and nodemon installed to handle form requests and server refreshing, respectively

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-console.log("Hello from server.js");
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.listen(port, () => {
+    console.log('listening on port ' + port);
+})
+
+// simple get request that sends "hello world" to the DOM
+// app.get('/', (req, res) => {
+//     res.send("Hello World");
+// })
+
+app.get('/', (req, res) => {
+    // __dirname is where you are currently in the folders
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.post("/quotes", (req, res) => {
+    console.log(req.body);
+})
+
+console.log(__dirname);
+console.log("Hello from server.js with nodemon!");
